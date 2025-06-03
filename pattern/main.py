176c66faef6e6a00137b36f662d3e6677df3e9b3
sandeep kumar <43428387-indusdocx@users.noreply.replit.com>
2025-06-03 15:46:59 +0000

    square hollow pattern

---
+++ pattern/main.py
@@ -0,0 +1,10 @@
+for i in range(1, 7):
+  for j in range(1, 7):
+    if (i == 1 or i == 6):
+      print("*", end=" ")
+    else:
+      if j == 1 or j == 6:
+        print("*", end=" ")
+      else:
+        print(" ", end=" ")
+  print()
